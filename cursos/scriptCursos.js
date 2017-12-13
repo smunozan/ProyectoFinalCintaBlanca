0a36ac42d6bf39ed8e71efa9b11c0d8b90f3341c
smunozan <sebasmunoz694@gmail.com>
2017-12-13 00:49:45 -0600

    actualizacion 12:50

--- cursos/scriptCursos.js
+++ cursos/scriptCursos.js
@@ -1,13 +1,17 @@
+// boton de inicio
+
 $("#empezar").click(function(){
 	$(window).scrollTop($('#main').offset().top);
 })
 
-// -----------------
+// filtros
 
 $("#desarrolloWeb").hover(function(){
 	$("#menuDesarrolloWeb").show()
 	$("#menuAplicacionesMoviles").hide()
 	$("#menuLenguajesProgramacion").hide()
+	$("#menuVideojuegos").hide()
+	$("#menuMasOpciones").hide()
 })
 
 $("#menuDesarrolloWeb").hover(function(){
@@ -22,6 +26,8 @@ $("#aplicacionesMoviles").hover(function(){
 	$("#menuAplicacionesMoviles").show()
 	$("#menuDesarrolloWeb").hide()
 	$("#menuLenguajesProgramacion").hide()
+	$("#menuVideojuegos").hide()
+	$("#menuMasOpciones").hide()
 })
 
 $("#menuAplicacionesMoviles").hover(function(){
@@ -36,6 +42,8 @@ $("#LenguajesProgramacion").hover(function(){
 	$("#menuLenguajesProgramacion").show()
 	$("#menuDesarrolloWeb").hide()
 	$("#menuAplicacionesMoviles").hide()
+	$("#menuVideojuegos").hide()
+	$("#menuMasOpciones").hide()
 })
 
 $("#menuLenguajesProgramacion").hover(function(){
@@ -44,3 +52,52 @@ $("#menuLenguajesProgramacion").hover(function(){
 	$("#menuLenguajesProgramacion").hide()
 })
 
+// -----------------
+
+$("#videojuegos").hover(function(){
+	$("#menuVideojuegos").show()
+	$("#menuDesarrolloWeb").hide()
+	$("#menuAplicacionesMoviles").hide()
+	$("#menuLenguajesProgramacion").hide()
+	$("#menuMasOpciones").hide()
+})
+
+$("#menuVideojuegos").hover(function(){
+	$("#menuVideojuegos").show()
+},function() {
+	$("#menuVideojuegos").hide()
+})
+
+// -----------------
+
+$("#masOpciones").hover(function(){
+	$("#menuMasOpciones").show()
+	$("#menuVideojuegos").hide()
+	$("#menuDesarrolloWeb").hide()
+	$("#menuAplicacionesMoviles").hide()
+	$("#menuLenguajesProgramacion").hide()
+})
+
+$("#menuMasOpciones").hover(function(){
+	$("#menuMasOpciones").show()
+},function() {
+	$("#menuMasOpciones").hide()
+})
+
+// funcionamiento de filtros
+
+
+// detalle de cursos
+
+$("#curso1").click(function(){
+	$("#cajaDetalles1").css("display", "flex")
+})
+
+$("#cajaDetalles1").click(function(){
+	$("#cajaDetalles1").css("display", "none")
+})
+
+$("#empezar1").click(function(){
+	 window.open("https://www.udemy.com/programacion-de-android-desde-cero/");
+    return false;
+  })
