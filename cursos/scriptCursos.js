a4200d212139c33f53b232d8a27b352497c6572b
smunozan <sebasmunoz694@gmail.com>
2017-12-12 21:55:54 -0600

    Fin dia 2

--- cursos/scriptCursos.js
+++ cursos/scriptCursos.js
@@ -2,10 +2,45 @@ $("#empezar").click(function(){
 	$(window).scrollTop($('#main').offset().top);
 })
 
+// -----------------
+
 $("#desarrolloWeb").hover(function(){
 	$("#menuDesarrolloWeb").show()
-},
-// function() {
-// 	$("#menuDesarrolloWeb").hide()
-// }
-)
+	$("#menuAplicacionesMoviles").hide()
+	$("#menuLenguajesProgramacion").hide()
+})
+
+$("#menuDesarrolloWeb").hover(function(){
+	$("#menuDesarrolloWeb").show()
+},function() {
+	$("#menuDesarrolloWeb").hide()
+})
+
+// -----------------
+
+$("#aplicacionesMoviles").hover(function(){
+	$("#menuAplicacionesMoviles").show()
+	$("#menuDesarrolloWeb").hide()
+	$("#menuLenguajesProgramacion").hide()
+})
+
+$("#menuAplicacionesMoviles").hover(function(){
+	$("#menuAplicacionesMoviles").show()
+},function() {
+	$("#menuAplicacionesMoviles").hide()
+})
+
+// -----------------
+
+$("#LenguajesProgramacion").hover(function(){
+	$("#menuLenguajesProgramacion").show()
+	$("#menuDesarrolloWeb").hide()
+	$("#menuAplicacionesMoviles").hide()
+})
+
+$("#menuLenguajesProgramacion").hover(function(){
+	$("#menuLenguajesProgramacion").show()
+},function() {
+	$("#menuLenguajesProgramacion").hide()
+})
+
